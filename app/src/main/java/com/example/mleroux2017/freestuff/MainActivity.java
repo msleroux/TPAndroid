7c5b89c01df7e37c673632c49aebf1ed62f8607a
Unknown <ml.maroux@gmail.com>
2018-06-22 17:01:51 +0200

    vendredi marine

---
+++ app/src/main/java/com/example/mleroux2017/freestuff/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mleroux2017.freestuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mleroux2017.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
