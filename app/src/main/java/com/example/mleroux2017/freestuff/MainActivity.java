6355e63cdbfe3470ee2518dd36b6f6ff71c250b8
Unknown <ml.maroux@gmail.com>
2018-06-25 10:47:19 +0200

    add CategoryController + insert

--- app/src/main/java/com/example/mleroux2017/freestuff/MainActivity.java
+++ app/src/main/java/com/example/mleroux2017/freestuff/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mleroux2017.freestuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mleroux2017.freestuff.Controllers.CategoryController;
 import com.example.mleroux2017.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CategoryController.insert(new Categorie("Meubles"));
     }
 }
