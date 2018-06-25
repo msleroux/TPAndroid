dab905aaa70b5fca26ddf9c8e9708eac84b7d9fc
Unknown <ml.maroux@gmail.com>
2018-06-25 16:22:27 +0200

    add getByTitre et getAll

--- app/src/main/java/com/example/mleroux2017/freestuff/MainActivity.java
+++ app/src/main/java/com/example/mleroux2017/freestuff/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.mleroux2017.freestuff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.example.mleroux2017.freestuff.Controllers.CategoryController;
+import com.example.mleroux2017.freestuff.Controllers.CategorieControllerFirebase;
 import com.example.mleroux2017.myapplication.R;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CategoryController.insert(new Categorie("Meubles"));
+        //CategorieControllerFirebase.insert(new Categorie("Beaute"));
+
+        CategorieControllerFirebase.getAll(new CategorieControllerFirebase.OnTabListener() {
+          @Override
+          public void onGetTabListener(ArrayList<Categorie> tab) {
+              //écrire ici ce qu'on fait de la liste
+              for (Categorie c :tab)
+              {
+                  Toast.makeText(MainActivity.this,"titre"+c.getTitre(), Toast.LENGTH_LONG).show();
+              }
+          }
+        });
+
+
+        CategorieControllerFirebase.getByTitre(new CategorieControllerFirebase.OnValueListener() {
+                                                   @Override
+                                                   public void onGetValueListener(Categorie cat) {
+                                                       Log.i("doc","main"+cat.getTitre());
+
+                                                   }
+                                               },"Beaute");
+
+        //CategorieControllerFirebase.delete(cat);
+
     }
 }
